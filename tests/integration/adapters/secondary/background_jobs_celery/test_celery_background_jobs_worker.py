c7d027882d91cee2c3d8163ebe7ebdb3014cce56
Liam Considine <lconsidine@schmidtentitiesprojects.org>
2024-07-28 13:23:27 -0400

    Save changes

--- tests/integration/adapters/secondary/background_jobs_celery/test_celery_background_jobs_worker.py
+++ tests/integration/adapters/secondary/background_jobs_celery/test_celery_background_jobs_worker.py
@@ -2,7 +2,7 @@ from sensor_app.core.domain.entities import Sensor
 from sensor_app.core.domain.results import AsyncResult
 
 
-def test_send_task(sensor_repo, background_jobs_repo, test_logger):
+def test_send_task(background_jobs_repo, test_logger):
     results = background_jobs_repo.send_task("make_one_thousand_sensors", count=5)
     assert isinstance(results, AsyncResult)
     assert len(results.result) == 5
